18af783816a0c020edfc977532f144b0e0c66618
yoichiro <youichirokaneko@gmail.com>
2020-03-08 19:02:02 +0900

    change seeder file
    add dummy data on seeder file

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
         	UsersTableSeeder::class,
         	CategoriesTableSeeder::class,
         	PostsTableSeeder::class,
+            CommentsTableSeeder::class,
         ]);
     }
 }
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Carbon\Carbon;
 
 class UsersTableSeeder extends Seeder
 {
@@ -16,12 +17,25 @@ class UsersTableSeeder extends Seeder
                 'name' => 'ichiro',
             	'password' =>  bcrypt('ichiroichiro'),
             	'email' => 'ichiro@ichiro.com',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' =>  date('Y-m-d H:i:s'),
         	],
             [
+
                 'name' => 'yoyo', 
                 'password' => bcrypt('yoyoyoyo'),
                 'email' => 'yoyo@yoyo.com',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' =>  date('Y-m-d H:i:s'),
             ],
+            [
+
+                'name' => 'xian',
+                'password' => bcrypt('xianxian'),
+                'email' => 'xian@xian.com',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' =>  date('Y-m-d H:i:s'),
+            ]
         ]);
     }
 }
